71ac728ad9a43a0dabe8f610a6d571cda64dcb2f
pythonai250824 <pythonai250824@gmail.com>
2025-01-29 21:42:10 +0200

    upload

---
+++ targil.py
@@ -0,0 +1,17 @@
+# 1. add function for execute_read_query -- Done.
+# 2. write in a loop, until 'exit', or once:
+#      input username
+#      input password
+#      insert these values into the table
+#          login_times = 0
+# 3. write in a loop, until 'exit', or once:
+#      input username
+#      input password
+#      check if the user-password correct
+#         print "login was successful"
+#       bonus:
+#       if not, check if the user exist-
+#         if so print "wrong pwd"
+#         if not print "user does not exist"
+#      after successful login (update query):
+#         add 1 into the user login_amount
